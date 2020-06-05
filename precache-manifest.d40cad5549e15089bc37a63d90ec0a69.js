5b8e0931beddfa844b54f936487c2852af67fb59
vipul <vipul9556@gmail.com>
2020-06-05 11:03:23 +0530

    Updates

---
+++ precache-manifest.d40cad5549e15089bc37a63d90ec0a69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35cb87f2ad96a8cda11bd705dff107d",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fe7ac36e7b0bdcd1da16",
+    "url": "/RoboFriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/RoboFriends/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7ac36e7b0bdcd1da16",
+    "url": "/RoboFriends/static/js/main.585858fa.chunk.js"
+  },
+  {
+    "revision": "af258257253a0ffa7f4f",
+    "url": "/RoboFriends/static/js/runtime-main.288d7cdc.js"
+  }
+]);
